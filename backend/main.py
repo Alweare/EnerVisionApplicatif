ffb9f740fcd1639530b5df71800a370a96dc852e
Alweare <yanis.dumont@gmail.com>
2026-09-01 13:55:20 +0200

    [TECH] (refactor): renommage de app en backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.health.api.routes import router as health_router
+from backend.health.api.routes import router as health_router
 
 app = FastAPI(
     title="EnerVision API",
