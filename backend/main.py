46d4722d27e0fb84a443a89d4cbd068c7f4021fc
ATondusson <alexis.nicolasherman2023@campus-eni.fr>
2026-09-02 10:40:10 +0200

    EN-88 creation des routes get_sites et get_site_detail. Création des tests unitaire correspondant et mock de la DB

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from backend.health.api.routes import router as health_router
+from backend.sites.api.routes import router as sites_router
 
 app = FastAPI(
     title="EnerVision API",
@@ -9,3 +10,4 @@ app = FastAPI(
 
 #Déclarer les routes ici, dans chaque module faire un routes.py sur le modèle de health
 app.include_router(health_router)
+app.include_router(sites_router)
