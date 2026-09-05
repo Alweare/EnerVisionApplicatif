fb97ee37bbe015a6ee7673ccca35c1806a4f2819
Alice Champigny <champigny.alice@gmail.com>
2026-09-05 17:38:48 +0200

    déplace database dans dossier partagé

---
+++ backend/shared/database.py
@@ -0,0 +1,20 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+POSTGRES_DB = os.environ["POSTGRES_DB"]
+POSTGRES_APP_USER = os.environ["POSTGRES_APP_USER"]
+POSTGRES_APP_PWD = os.environ["POSTGRES_APP_PWD"]
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_APP_USER}:{POSTGRES_APP_PWD}@postgres:5432/{POSTGRES_DB}"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
